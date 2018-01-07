37c182e0a902a1bc0a214bed659119f2715f7327
gabo <gabo696hn@gmail.com>
2018-01-06 20:36:33 -0400

    Added comments.

--- mysite/settings.py
+++ mysite/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-us'
 
 TIME_ZONE = 'America/Caracas'
 
@@ -121,3 +121,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOGIN_REDIRECT_URL = '/'
